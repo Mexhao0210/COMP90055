ddc159a5e16b1975a8fd9313c978ed2a8a7443c4
Kevin <39933318+kevinluoxinkai@users.noreply.github.com>
2020-05-09 17:47:25 +1000

    version 1.3

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,9 @@
 var Product = artifacts.require("Product");
 var supplyBlock = artifacts.require("supplyBlock");
+var Main = artifacts.require("Main");
 
 module.exports = function(deployer) {
+  deployer.deploy(Main);
   deployer.deploy(Product);
-};
-
-module.exports = function(deployer) {
   deployer.deploy(supplyBlock);
 };
